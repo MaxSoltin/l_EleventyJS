2b454a2199357efb5140ff9898703e97d76d7061
Max Soltin <timax.pxu@gmail.com>
2021-09-27 12:12:23 +0300

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,14 @@
-module.exports = function(eleventyConfig){
-  return {
-    addPassthroughFileCopy: true,
-    markdownTemplateEngine: 'njk', 
-    templateFormats: ['html', 'njk', 'md'],
-    dir: {
-      input: 'src',
-      output: '_site',
-      includes: 'includes',
-      layouts: 'layouts',
-      data: 'data'
-    }
-  }
+module.exports = function (eleventyConfig) {
+	return {
+		addPassthroughFileCopy: true,
+		markdownTemplateEngine: 'njk',
+		templateFormats: ['html', 'njk', 'md'],
+		dir: {
+			input: 'src',
+			output: '_site',
+			includes: 'includes',
+			layouts: 'layouts',
+			data: 'data'
+		}
+	}
 }
